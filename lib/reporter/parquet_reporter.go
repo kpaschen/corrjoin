b66dfbcdc8a0778c1f20708f0d11f765e3847577
K Paschen <kathrin@paschen.ch>
2025-01-28 17:29:45 +0100

    experiment with parquet

---
+++ lib/reporter/parquet_reporter.go
@@ -0,0 +1,151 @@
+package reporter
+
+import (
+  "encoding/json"
+	"fmt"
+	"github.com/kpaschen/corrjoin/lib/datatypes"
+	"github.com/kpaschen/corrjoin/lib/settings"
+  "github.com/parquet-go/parquet-go"
+  "github.com/prometheus/common/model"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+)
+
+// TODO: extract for reusability
+type Timeseries struct {
+  ID int  `parquet:"id"`
+  Metric string `parquet:"metric,optional,zstd"`
+  Labels map[string]string `parquet:"labels,optional"`
+
+  // Cannot make this optional, as then '0' will be written as null.
+  // Instead, when you want to say "no correlation information", leave the Pearson
+  // field blank and set Correlated to be the same as ID.
+  Correlated int `parquet:"correlated"` 
+  // There is no float16 datatype in go, but maybe a fixed-precision representation would be best.
+  Pearson float32 `parquet:"pearson,optional"`
+  Constant bool `parquet:"constant,optional"`
+}
+
+type ParquetReporter struct {
+	filenameBase     string
+	tsids            []string
+	strideStartTimes map[int]string
+	strideEndTimes   map[int]string
+  writer *parquet.SortingWriter[Timeseries]
+  filepath string
+}
+
+func NewParquetReporter(filenameBase string) *ParquetReporter {
+	return &ParquetReporter{
+		filenameBase:     filenameBase,
+		strideStartTimes: make(map[int]string),
+		strideEndTimes:   make(map[int]string),
+    writer: nil,
+    filepath: "",
+	}
+}
+
+func (r *ParquetReporter) Initialize(config settings.CorrjoinSettings, strideCounter int,
+	strideStart time.Time, strideEnd time.Time, tsids []string) {
+	r.tsids = tsids
+	r.strideStartTimes[strideCounter] = strideStart.UTC().Format("20060102150405")
+	r.strideEndTimes[strideCounter] = strideEnd.UTC().Format("20060102150405")
+	log.Printf("initializing with strideCounter %d, start time %s (%s), end time %s (%s)\n",
+		strideCounter, r.strideStartTimes[strideCounter], strideStart.UTC().String(),
+		r.strideEndTimes[strideCounter], strideEnd.UTC().String())
+
+	startTime := r.strideStartTimes[strideCounter]
+	endTime := r.strideEndTimes[strideCounter]
+	filename := fmt.Sprintf("correlations_%d_%s-%s.pq", strideCounter, startTime, endTime)
+	r.filepath = filepath.Join(r.filenameBase, filename)
+
+  file, err := os.OpenFile(r.filepath, os.O_WRONLY|os.O_CREATE, 0640)
+  if err != nil {
+    log.Printf("failed to open ts parquet file: %e\n", err)
+    return
+  }
+
+  r.writer = parquet.NewSortingWriter[Timeseries](file, 1000,
+    parquet.SortingWriterConfig(
+      parquet.SortingColumns(
+        parquet.Ascending("id"),
+      ),
+    ),
+  )
+
+  metadataRows := make([]Timeseries, len(tsids), len(tsids))
+  for i, tsid := range tsids {
+     var metricModel model.Metric
+
+     // This copies tsid. It might be more efficient to store byte slices
+     // in the tsids array instead?
+     err := json.Unmarshal(([]byte)(tsid), &metricModel)
+     if err != nil {
+        log.Printf("failed to unmarshal tsid %s: %e\n", tsid, err)
+        return
+     }
+     row := Timeseries{
+        ID: i,
+        Correlated: i, // See above, this field is not optional.
+        Metric: string(metricModel["__name__"]),
+        Labels: make(map[string]string),
+     }
+     for key, value := range metricModel {
+        if key == "__name__" { continue }
+        row.Labels[string(key)] = string(value)
+     }
+     metadataRows[i] = row
+  }
+  r.writer.Write(metadataRows)
+}
+
+func extractRowsFromResult(result datatypes.CorrjoinResult) []Timeseries {
+   retsize := 2 * len(result.CorrelatedPairs)
+   ret := make([]Timeseries, retsize, retsize)
+
+   ctr := 0
+   for pair, pearson := range result.CorrelatedPairs {
+      rowids := pair.RowIds()
+      ts1 := Timeseries{
+         ID: rowids[0],
+         Correlated: rowids[1],
+         Pearson: float32(pearson),
+      }
+      ret[ctr] = ts1
+      ctr++
+      ts2 := Timeseries{
+         ID: rowids[1],
+         Correlated: rowids[0],
+         Pearson: float32(pearson),
+      }
+      ret[ctr] = ts2
+      ctr++
+   }
+
+   return ret
+}
+
+func (r *ParquetReporter) AddCorrelatedPairs(result datatypes.CorrjoinResult) error {
+  if r.writer == nil {
+     return fmt.Errorf("missing writer for timeseries")
+  }
+
+  rows := extractRowsFromResult(result)
+
+  n, err := r.writer.Write(rows)
+  if err != nil {
+     log.Printf("correlated pairs writer returned %d\n", n)
+  }
+  
+	return err
+}
+
+func (r *ParquetReporter) Flush() error {
+   if r.writer == nil {
+      return nil
+   }
+   defer r.writer.Close()
+   return r.writer.Flush()
+}
---
+++ lib/reporter/parquet_reporter_test.go
@@ -0,0 +1,138 @@
+package reporter
+
+import (
+  "encoding/json"
+  "fmt"
+  "os"
+  "strings"
+	"testing"
+  "time"
+  "github.com/kpaschen/corrjoin/lib/datatypes"
+  "github.com/kpaschen/corrjoin/lib/settings"
+  "github.com/parquet-go/parquet-go"
+  "github.com/prometheus/common/model"
+)
+
+func TestInitialize(t *testing.T) {
+  tempdir, err := os.MkdirTemp("", "corrjoinTest")
+  if err != nil {
+     t.Errorf("failed to create temp dir")
+  }
+  defer os.RemoveAll(tempdir)
+	rep := NewParquetReporter(tempdir)
+ 
+  sampleMetric := model.Metric{}
+  sampleMetric[model.LabelName("__name__")] = model.LabelValue("myMetric") 
+  sampleMetric[model.LabelName("pod")] = model.LabelValue("podxy") 
+  sampleMetricString, err := json.Marshal(sampleMetric)
+  if err != nil { t.Errorf("failed to marshal sample metric") }
+
+  sampleMetric = model.Metric{}
+  sampleMetric[model.LabelName("__name__")] = model.LabelValue("myOtherMetric") 
+  sampleMetric[model.LabelName("pod")] = model.LabelValue("podab") 
+  sampleMetricString2, err := json.Marshal(sampleMetric)
+  if err != nil { t.Errorf("failed to marshal sample metric") }
+
+  settings := settings.CorrjoinSettings{
+    SvdDimensions:        3,
+    EuclidDimensions:     2,
+    CorrelationThreshold: 0.9,
+    WindowSize:           3,
+    SvdOutputDimensions:  2,
+  }.ComputeSettingsFields()
+  rep.Initialize(settings, 1, time.Now(), time.Now(), []string{string(sampleMetricString), string(sampleMetricString2)})
+
+  rep.Flush()
+}
+
+func TestAddCorrelatedPairs(t *testing.T) {
+  tempdir, err := os.MkdirTemp("", "corrjoinTest")
+  if err != nil {
+     t.Errorf("failed to create temp dir")
+  }
+  fmt.Printf("temp dir: %s\n", tempdir)
+  // defer os.RemoveAll(tempdir)
+	rep := NewParquetReporter(tempdir)
+ 
+  sampleMetric := model.Metric{}
+  sampleMetric[model.LabelName("__name__")] = model.LabelValue("myMetric") 
+  sampleMetric[model.LabelName("pod")] = model.LabelValue("podxy") 
+  sampleMetricString, err := json.Marshal(sampleMetric)
+  if err != nil { t.Errorf("failed to marshal sample metric") }
+
+  sampleMetric = model.Metric{}
+  sampleMetric[model.LabelName("__name__")] = model.LabelValue("myOtherMetric") 
+  sampleMetric[model.LabelName("pod")] = model.LabelValue("podab") 
+  sampleMetricString2, err := json.Marshal(sampleMetric)
+  if err != nil { t.Errorf("failed to marshal sample metric") }
+
+  settings := settings.CorrjoinSettings{
+    SvdDimensions:        3,
+    EuclidDimensions:     2,
+    CorrelationThreshold: 0.9,
+    WindowSize:           3,
+    SvdOutputDimensions:  2,
+  }.ComputeSettingsFields()
+  rep.Initialize(settings, 1, time.Now(), time.Now(), []string{string(sampleMetricString), string(sampleMetricString2)})
+
+  results1 := datatypes.CorrjoinResult{
+     CorrelatedPairs: make(map[datatypes.RowPair]float64),
+     StrideCounter: 1,
+  }
+  rp1 := datatypes.RowPair{1, 2}
+  results1.CorrelatedPairs[rp1] = 0.95
+
+  rp2 := datatypes.RowPair{0, 2}
+  results1.CorrelatedPairs[rp2] = 0.99
+
+  err = rep.AddCorrelatedPairs(results1)
+  if err != nil { t.Errorf("failed to add correlated pair") }
+
+  rep.Flush()
+
+  var idIndex, correlatedIndex, pearsonIndex int
+  schema := parquet.SchemaOf(Timeseries{})
+  for _, path := range schema.Columns() {
+    leaf, _ :=  schema.Lookup(path...)
+    v := strings.Join(path, ".")
+    if v == "id" { idIndex = leaf.ColumnIndex }
+    if v == "correlated" { correlatedIndex = leaf.ColumnIndex }
+    if v == "pearson" { pearsonIndex = leaf.ColumnIndex }
+    fmt.Printf("%d -> %q\n", leaf.ColumnIndex, strings.Join(path, "."))
+  }
+
+  pqfile, err := os.Open(rep.filepath)
+  if err != nil { t.Errorf("failed to open results file") }
+  stat, _ := pqfile.Stat()
+  p, err := parquet.OpenFile(pqfile, stat.Size())
+  if err != nil { t.Errorf("parquet failed to open results file") }
+  
+  for _, rg := range p.RowGroups() {
+     fmt.Printf("handling row group %+v\n", rg)
+     idchunk := rg.ColumnChunks()[idIndex]
+     ididx, _ := idchunk.ColumnIndex()
+     found := parquet.Find(ididx, parquet.ValueOf(0), parquet.CompareNullsLast(idchunk.Type().Compare)) 
+     if found == ididx.NumPages() {
+        fmt.Println("page for id 0 not found in current row group")
+        continue
+     }
+     fmt.Printf("find returned page %d for id 0\n", found)
+     
+     correlatedChunk := rg.ColumnChunks()[correlatedIndex]
+     pearsonChunk := rg.ColumnChunks()[pearsonIndex]
+     _ = correlatedChunk
+     _ = pearsonChunk
+     reader := parquet.NewGenericReader[Timeseries](pqfile) 
+     defer reader.Close()
+     offsetidx, _ := idchunk.OffsetIndex()
+     reader.SeekToRow(offsetidx.FirstRowIndex(found))
+     result := make([]Timeseries, 10)
+     // Read the next rows into the given rows slice
+     // returns the number of rows read and an io.EOF when no more rows
+     _, _ = reader.Read(result)
+     for _, results := range result {
+        // Need to check these results for a match on the id column
+        fmt.Printf("row in range: %v\n", results)
+     }
+  }
+}
